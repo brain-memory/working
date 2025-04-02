324cd94c5e947e54cd073226996ffba28e2e81ea
Amine Amazou <amazou@outlook.fr>
2025-04-02 22:43:34 +0000

    feat: Create the working function with the key and callback and store the results on the temporary memory depends on the args

--- dist/working.js
+++ dist/working.js
@@ -1,5 +1,18 @@
 ;(function(b) {
-    b.working = function() {
-        console.log('Welcome from the working plugin!')
+
+    function noop() {};
+
+    b.working = function(key, callback = noop) {
+        const memo = BrainMemory.temporary('__working__' +   key);
+        
+        return function(...args) {
+            if(memo.hasRecord(args.join(','))) {
+                return memo.recall(args.join(','))
+            }
+            let result = callback(...args);
+            memo.record(args.join(','), result);
+            return result;
+        }
     }
+
 }(BrainMemory));
